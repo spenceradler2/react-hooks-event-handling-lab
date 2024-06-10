ea329b45e294566bd0ac671c23d1c3207d822472
Spencer <spencer.adler2@gmail.com>
2024-06-10 10:25:17 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
+
+
 ReactDOM.render(
+  
   <div>
-    <Keypad />
-    <EyesOnMe />
+    <Keypad/>
+    <EyesOnMe/>
   </div>,
   document.getElementById('root')
-);
+)
